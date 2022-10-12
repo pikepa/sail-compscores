b4d947377d5c9f085cf03f04ea38d3ef192ea40d
Peter Pike <pikepeter@gmail.com>
2022-10-12 16:47:44 +0800

    Updated to group routes under auth middleware

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pages.welcome');
-})->name('welcome');
+    })->name('welcome');
 Route::get('/profile', function () {
     return view('pages.profile');
-})->name('profile');
+    })->name('profile');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/organisation', ManageOrganisations::class)->name('organisation');
